2c90b2cbed78be881fb25033404ee49fdb7bf40b
rifat32 <rifatbilalphilips@gmail.com>
2025-08-16 20:59:43 +0600

    feat: Add fillable properties and relationships to LandlordPayableRent and RentAdjustment models

--- app/Models/LandlordPayableRent.php
+++ app/Models/LandlordPayableRent.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class LandlordPayableRent extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'landlord_rent_payable_id',
+        'rent_id',
+    ];
+
+    // Relationships
+    public function landlord_rent_payable()
+    {
+        return $this->belongsTo(LandlordRentPayable::class);
+    }
+
+    public function rent()
+    {
+        return $this->belongsTo(Rent::class);
+    }
 }
--- app/Models/RentAdjustment.php
+++ app/Models/RentAdjustment.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class RentAdjustment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'landlord_rent_payable_id',
+        'amount',
+        'description',
+    ];
+
+    // Relationships
+    public function landlord_rent_payable()
+    {
+        return $this->belongsTo(LandlordRentPayable::class);
+    }
 }
